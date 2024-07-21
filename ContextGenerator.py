08feaf947364d994fae3bf71f34529dc69aa3ec0
Oliver <oligo99@t-online.de>
2024-07-21 18:55:55 +0200

    sts

--- ContextGenerator.py
+++ ContextGenerator.py
@@ -1,7 +1,12 @@
 import tiktoken
 import warnings
 
+from Protocol import protocol_obj
 
+
+# Die Aufgabe der Klasse besteht darin, die gesammelten Kontext-Fenster zu sortieren, als natürlich erscheinende Kommentare
+# in den Kontext-String einzubetten und den Kontext zu reduzieren, falls die Anzahl der Tokens die Größe des Kontext-Fensters
+# überschreitet.
 class ContextGenerator:
 
     def __init__(self, windows, path, cursor_position, context_token_limit):
@@ -16,14 +21,17 @@ class ContextGenerator:
 
         context_from_other_files = ""
         context_of_current_file = self.__get_context_from_current_file()
+        protocol_obj.context_of_current_file = context_of_current_file
         for window in self.windows:
             context_from_other_files += self.__to_comment(window) + "\n\n"
 
         return self.reduce_context_if_necessary(context_from_other_files, context_of_current_file)
 
+    # Code-Ausschnitte mit einem hohen übereinstimmungswert sollen dem Cursor am nächsten stehen
     def __align_snippets(self):
         self.windows.sort(key=lambda x: x['value'], reverse=True)
 
+    # Funktion, die die Code-Ausschnitte als Kommentare präpariert
     def __to_comment(self, window):
         language_specific_comment = "//"
         first_line = language_specific_comment + "compare this snippet from " + window['file_path'] + ":\n"
@@ -32,6 +40,7 @@ class ContextGenerator:
         code = first_line + "\n".join(code)
         return code
 
+    # Funktion, die den Kontext des aktuellen Files zurückgibt und an der Stelle des Cursors "<insert Code here>" einfügt
     def __get_context_from_current_file(self):
         with open(self.path, 'r') as f:
             code = f.read()
@@ -41,20 +50,28 @@ class ContextGenerator:
         context = "\n".join(lines)
         return context
 
-    def reduce_context_if_necessary(self, context_from_other_files, context_of_current_file):
+    # Funktion, die den Kontext reduziert, sofern der gesammelte Kontext die größe des Kontext-Fensters überschreitet
+    # Der Kontext wird um 10 Zeilen reduziert, bis die Anzahl der Tokens des Kontexts unter dem Limit liegt.
+    def reduce_context_if_necessary(self, context_from_other_files, context_of_current_file): #todo protokol
         tokens_of_current_file = self.num_tokens_from_string(context_of_current_file)
         tokens_of_other_files = self.num_tokens_from_string(context_from_other_files)
+        protocol_obj.num_tokens_of_current_file = tokens_of_current_file
+        protocol_obj.num_tokens_of_other_files = tokens_of_other_files
         if tokens_of_current_file > self.context_token_limit:
             warnings.warn("Das aktuelle File enthält zu viele Tokens")
+            protocol_obj.is_valid = False
+        protocol_obj.removed_lines_from_context = ''
         if tokens_of_other_files + tokens_of_current_file > self.context_token_limit:
             while tokens_of_other_files + tokens_of_current_file > self.context_token_limit:
+                removed_lines_from_context = context_from_other_files[-10:] #context_from_other_files.split("\n")[-10:] TODO was splitten wir hier ?
+                protocol_obj.removedLinesFromContext = removed_lines_from_context + "\n" + protocol_obj.removed_lines_from_context
                 context_from_other_files = context_from_other_files[:-10]
                 tokens_of_other_files = self.num_tokens_from_string(context_from_other_files)
 
         return context_from_other_files + "\n" + context_of_current_file
 
+    # Funktion, die die Anzahl der Tokens von einem Code-Bereich zurückgibt
     def num_tokens_from_string(self, code):
-        """Returns the number of tokens in a text string."""
         encoding = tiktoken.get_encoding(self.encoding_name)
         num_tokens = len(encoding.encode(code))
         return num_tokens
--- OpenAIClient.py
+++ OpenAIClient.py
@@ -1,45 +1,23 @@
 import openai
-from openai import OpenAI
 
 
 class OpenAIClient:
 
-    def __init__(self, context, openai_key):
+    def __init__(self, context, openai_key, model):
         self.context = context
         openai.api_key = openai_key
+        self.model = model
 
+    # Funktion, die den Prompt an OpenAI sendet und die Antwort zurückgibt
     def submit_prompt(self):
-        # create two variables, one for the substring in self.context that ist before <insert Code here>, and the other for the substring after <insert Code here>
-        before = self.context.split("<insert Code here>")[0]
-        after = self.context.split("<insert Code here>")[1]
-
-        print("before: ", before)
-        print("after: ", after)
-
-        client = OpenAI()
-        # response = client.completions.create(
-        #     model="gpt-3.5-turbo-instruct",
-        #     prompt=before + "\n" + "//insert Code here",
-        #     suffix=after,
-        #     max_tokens=100
-        # )
-        # return response.choices[0].text.strip()
-        # completion = openai.chat.completions.create(
-        #     model="gpt-3.5-turbo-0125",
-        #     messages=[
-        #         {"role": "system", "content": "You are a Programmer who is an expert in the field of context-usage."},
-        #         {"role": "user", "content": "insert code where <insert Code here> is and return only the inserted code: " + self.context}
-        #     ]
-        # )
         prompt_instruction = "insert code where <insert Code here> is and return only the inserted code: "
 
         completion = openai.chat.completions.create(
-            model="GPT-3.5-turbo-0125",
+            model=self.model,
             messages=[
                 {"role": "system", "content": "You are a Programmer who is an expert in the field of context-usage."},
                 {"role": "user", "content": prompt_instruction + self.context},
             ],
             temperature=1.0,
         )
-        print("completion: ", completion)
         return completion.choices[0].message
